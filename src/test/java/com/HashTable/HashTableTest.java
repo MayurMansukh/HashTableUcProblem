fba45180109448b942b7a8938d1138b1d18eb23c
MayurMansukh <manojmansukh7004>
2021-03-16 14:44:25 +0530

    add logic to find frequancy of words

---
+++ src/test/java/com/HashTable/HashTableTest.java
@@ -0,0 +1,29 @@
+package com.HashTable;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class HashTableTest {
+    @Test
+      public void find_frequency_of_given_words(){
+
+        String sentence="To be or not to be";
+        HashTable<String,Integer> hashTable=new HashTable<>();
+
+        String[] words=sentence.toLowerCase().split(" ") ;
+
+        for(String word : words){
+            Integer value=hashTable.get(word);
+            if(value == null){
+                value=1;
+            }
+            else {
+                value=value+1;
+            }
+            hashTable.add(word,value);
+        }
+        int frequency = hashTable.get("be");
+        System.out.println(hashTable);
+        Assertions.assertEquals(2,frequency);
+    }
+
+}
